49f8f813d18954bddc23c820214ce07991c0172d
harlock024 <hadith024@gmail.com>
2025-04-02 19:08:58 -0600

    slider color added

--- src/components/player/player-controllers.tsx
+++ src/components/player/player-controllers.tsx
@@ -1,24 +1,42 @@
 import { Button } from "../../components/ui/button";
 import { Slider } from "../ui/slider";
-import { Play, ChevronLast, ChevronFirst } from "lucide-react";
+import { Play, Pause, ChevronLast, ChevronFirst } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export function PlayerControllers() {
+  const [isPaused, setIsPaused] = useState(false);
+
+  function togglePlayPause() {
+    setIsPaused(!isPaused);
+  }
+
   return (
     <div className="flex flex-col w-full items-center">
       <div className="flex justify-center space-x-3">
         <Button size="icon" variant="ghost" aria-label="Previous Track">
           <ChevronFirst />
         </Button>
-        <Button size="icon" variant="default" aria-label="Play/Pause">
-          <Play />
+        <Button
+          size="icon"
+          variant="default"
+          aria-label="Play/Pause"
+          onClick={togglePlayPause}
+        >
+          {isPaused ? <Play /> : <Pause />}
         </Button>
         <Button size="icon" variant="ghost" aria-label="Next Track">
           <ChevronLast />
         </Button>
       </div>
 
-      <div className="w-[60%] mt-3 px-4">
-        <Slider defaultValue={[33]} max={100} step={1} className="w-full" />
+      <div className="w-[60%]  mt-3 px-4">
+        <Slider
+          defaultValue={[33]}
+          max={100}
+          step={1}
+          className={cn("w-full", "")}
+        />
       </div>
     </div>
   );
